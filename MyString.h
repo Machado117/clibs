e8ab2707cf7f7846cc999eba7654329b3f6f2eaf
Diogo Machado <diogomachado@yandex.com>
2015-08-30 20:51:06 +0100

    Improved MyString code

--- MyString.h
+++ MyString.h
@@ -10,26 +10,27 @@
 struct String {
     int length_;
     int capacity_;
-    char *string_;
+    char *string_data_;
 };
 
-typedef struct String String;
+typedef struct String *String;
 
-ErrorCode CreateString(String *string, char *text);
-ErrorCode CreateEmptyString(String *string);
-void DestroyString(String *string);
-ErrorCode GrowString(String *string, int size_to_add);
-char *CString(String *string);
-ErrorCode AddCharString(String *string, char c);
-ErrorCode AddTextString(String *string, char *text);
-ErrorCode AddString(String *string_dest, String *string_src);
-int FindTextString(String *string, char *text, int index);
-void RemoveTextString(String *string, int index, int length);
-void ClearString(String *string);
-int LengthString(String *string);
-Bool EqualString(String *string1, String *string2);
-Bool EqualTextString(String *string, char *text);
-char GetLastChar(String *string);
-char GetCharString(String *string, int index);
+String CreateString(int minimum_capacity);
+String CreateStringFromText(char *text, int text_length);
+String CreateEmptyString();
+void DestroyString(String string);
+ErrorCode GrowString(String string, int new_minimum_capacity);
+char *CString(String string);
+ErrorCode AddCharString(String string, char c);
+ErrorCode AddTextString(String string, char *text, int text_length);
+ErrorCode AddString(String string_dest, String string_src);
+int FindTextString(String string, int index, char *text, int text_length);
+void RemoveTextString(String string, int index, int length);
+String GetSubString(String string, int index, int length);
+void ClearString(String string);
+int LengthString(String string);
+Bool EqualString(String string1, String string2);
+Bool EqualTextString(String string, char *text, int text_length);
+char GetCharString(String string, int index);
 
 #endif //CLIBS_MYSTRING_H
